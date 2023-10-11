92fe5dd98b7780f5c3f9c53fad8b2fd2b0729a6f
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-10-12 10:12:36 +1100

    Placeholder Commit

--- myPWA/.database/myQuery.sql
+++ myPWA/.database/myQuery.sql
@@ -1,3 +1,3 @@
 CREATE TABLE sddstudents (nesaID INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL);
 
---INSERT INTO sddstudents (nesaID, name, age) 
+--INSERT INTO sddstudents (nesaID, name, age)
