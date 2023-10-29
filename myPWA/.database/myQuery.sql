489b944f42fdd3b2b38be9cc7e3e35e765256857
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-10-30 10:02:52 +1100

    add webser

--- myPWA/.database/myQuery.sql
+++ myPWA/.database/myQuery.sql
@@ -1,3 +1,14 @@
-CREATE TABLE sddstudent1 (nesaID INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL);
+--CREATE TABLE sddstudent1 (nesaID INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL);
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (12324,"David Smith","17");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (27324,"Kei Rose","12");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (13732,"Ney Chen","14");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (36234,"Nathan Jone","18");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (63524,"Megan Deng","14");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (72938,"Peter Shawn","8");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (36271,"Calx Nyugen","20");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (46281,"David Jone","29");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (74821,"Peter Smith","2");
+--INSERT INTO sddstudent1 (nesaID, name, age) VALUES (26183,"Megan Chen","14");
+--SELECT * FROM sddstudent1;
 
---INSERT INTO sddstudents (nesaID, name, age)
+SELECT * FROM sddstudent1 WHERE name LIKE 'David Smith';
